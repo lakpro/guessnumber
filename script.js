9f76c91b3f454dcebd65e23de51ca4d3e8fbe74f
Lakshay Setia <73340463+lakpro@users.noreply.github.com>
2022-07-06 04:27:21 +0530

    Javascript added

--- script.js
+++ script.js
@@ -6,4 +6,37 @@ document.querySelector(".btn1").addEventListener("click", function () {
 document.querySelector(".btn2").addEventListener("click", function () {
   document.querySelector(".hide1").style.display = "flex";
   document.querySelector(".hide2").style.display = "none";
+  turns = 0;
+  document.querySelector(".score").textContent = turns;
+  secretNum = Math.trunc(Math.random() * 10) + 1;
+  document.querySelector(".btn3").disabled = false;
+  document.querySelector(".guess").value = "";
+});
+
+let secretNum = Math.trunc(Math.random() * 10) + 1;
+let turns = 0;
+let highScore = Infinity;
+console.log(secretNum);
+
+document.querySelector(".btn3").addEventListener("click", function () {
+  let curr = Number(document.querySelector(".guess").value);
+
+  if (curr === secretNum) {
+    document.querySelector(".msg1").textContent = "🎉 Correct Number!";
+    if (turns < highScore) {
+      highScore = turns;
+      document.querySelector(".hscore").textContent = turns;
+      document.querySelector(".btn3").disabled = true;
+    }
+  } else if (curr > secretNum) {
+    document.querySelector(".msg1").textContent = "Too High!";
+    turns++;
+    document.querySelector(".score").textContent = turns;
+  } else if (curr < secretNum) {
+    document.querySelector(".msg1").textContent = "Too Low!";
+    turns++;
+    document.querySelector(".score").textContent = turns;
+  } else {
+    document.querySelector(".msg1").textContent = "Invalid Input";
+  }
 });
