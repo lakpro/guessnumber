3a812c50803735ebf03b9e8cb241e7163c5654cf
Lakshay Setia <73340463+lakpro@users.noreply.github.com>
2022-07-06 02:53:10 +0530

    Front page complete

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn").addEventListener("click", function () {
+  document.querySelector(".heading").style.display = "none";
+  document.querySelector(".btn").style.display = "none";
+});
